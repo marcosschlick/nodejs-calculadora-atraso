e29945f43b7fef95071bfef3a56d7ab5a33e39ea
Marcos Schlick <marcosschlick@gmail.com>
2025-04-09 18:53:12 -0300

    Create debtController.js

---
+++ src/controllers/debtController.js
@@ -0,0 +1,40 @@
+import {
+  calcularMulta,
+  calcularJuros,
+  calcularValorFinal,
+} from "../services/debtService.js";
+
+export const calcularDividaCompleta = (req, res) => {
+  const { valorOriginal, taxaMulta, periodoMeses, taxaJurosMensal } = req.body;
+
+  if (
+    typeof valorOriginal !== "number" ||
+    typeof taxaMulta !== "number" ||
+    typeof periodoMeses !== "number" ||
+    typeof taxaJurosMensal !== "number"
+  ) {
+    return res.status(400).json({ erro: "Todos os valores devem ser números" });
+  }
+
+  if (
+    valorOriginal <= 0 ||
+    periodoMeses < 0 ||
+    taxaMulta < 0 ||
+    taxaJurosMensal < 0
+  ) {
+    return res.status(400).json({ erro: "Valores não podem ser negativos" });
+  }
+
+  const multa = calcularMulta(valorOriginal, taxaMulta);
+  const juros = Number(
+    calcularJuros(valorOriginal, periodoMeses, taxaJurosMensal),
+  ).toFixed(2);
+  const valorFinal = calcularValorFinal(valorOriginal, multa, juros);
+
+  res.json({
+    valorOriginal,
+    multa,
+    juros,
+    valorFinal,
+  });
+};
