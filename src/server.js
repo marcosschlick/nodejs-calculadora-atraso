7c0bece2884334ebdd22c8343f8262bd196d479b
Marcos Schlick <marcosschlick@gmail.com>
2025-04-22 20:52:23 -0300

    adicionar definição de port no `.env` suas dependências e no `.gitignore`

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
+import "dotenv/config";
 
-const PORT = 3000;
+const PORT = process.env.SV_PORT || 3333;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
