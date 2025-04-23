7bb65511ea17adb3ce9ad5e6fe9a35e52b05cd4e
Marcos Schlick <marcosschlick@gmail.com>
2025-04-23 20:23:47 -0300

    trocar portar no `script.js` e no `routes.http`

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ document.getElementById("debtForm").addEventListener("submit", async (e) => {
   const getVal = (id) => Number(document.getElementById(id).value);
 
   try {
-    const response = await fetch("http://localhost:3000/divida", {
+    const response = await fetch("http://localhost:3333/divida", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
